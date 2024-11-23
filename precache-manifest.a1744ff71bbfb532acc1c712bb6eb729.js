7004aaf8ed55e311959a49a87ce88f2a60f121d5
AdiTemp01 <adithya.s.001.4@gmail.com>
2024-11-23 12:27:23 +0530

    Updates

---
+++ precache-manifest.a1744ff71bbfb532acc1c712bb6eb729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f7f8e78cd2b560044ba3d309143f20",
+    "url": "/CAFToH/index.html"
+  },
+  {
+    "revision": "a80b29d89145fc2a624d",
+    "url": "/CAFToH/static/css/main.64d54b85.chunk.css"
+  },
+  {
+    "revision": "8d2a2940d6e7c2dda7ac",
+    "url": "/CAFToH/static/js/2.f5bf86b6.chunk.js"
+  },
+  {
+    "revision": "8b78294f34894140d628da3466e7f53b",
+    "url": "/CAFToH/static/js/2.f5bf86b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a80b29d89145fc2a624d",
+    "url": "/CAFToH/static/js/main.90b243b3.chunk.js"
+  },
+  {
+    "revision": "c04a1e2932e32f1fe497",
+    "url": "/CAFToH/static/js/runtime-main.0e1a0e1a.js"
+  }
+]);
